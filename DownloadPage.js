13761df7cec4ed7b1b319ca0c6bf8ffa2a78d99f
Ingvar <48952240+Ktulhovich@users.noreply.github.com>
2023-03-20 23:47:19 +0200

    update

---
+++ DownloadPage.js
@@ -0,0 +1,11 @@
+import "./DownloadPage.css";
+
+function DownloadPage() {
+  return (
+    <div className="DownloadPage">
+      <header className="download-page-header">
+        <input className="back-button" type="button" value="Back" />
+      </header>
+    </div>
+  );
+}
